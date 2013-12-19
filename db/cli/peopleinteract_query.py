dbc48d11854fac5a2d5faa2ce28a742014b31288
st0ll1 <st0ll1@yandex.ru>
2013-12-19 11:15:53 +0400

    Обновились

---
+++ db/cli/peopleinteract_query.py
@@ -0,0 +1,19 @@
+# интерактивные запросы
+import shelve
+
+fieldnames = ('name', 'age', 'job', 'pay')
+maxfield = max(len(f) for f in fieldnames)      # Извлекаем самое длинное поле
+db = shelve.open('class-shelve')
+
+while True: 
+    key = input('\nKey? => ')   # ключ или пустая строка, возбуждает исключение
+                                                 # при вводе EOF
+    if not key: break
+    
+    try:
+            record = db[key]         # извлечь запись по ключу и вывести
+    except:
+            print('No such key “%s”!' % key)
+    else:
+            for field in fieldnames:
+                print(field.ljust(maxfield), '=>', getattr(record, field))  # Извлекает атрибут объекта record по полю fields
---
+++ db/oop/update_db_classes.py
@@ -0,0 +1,11 @@
+# Обновление записей в базе используя классы
+
+import shelve
+db = shelve.open('class-shelve')
+sue = db['sue']
+sue.giveRaise(.25)
+db['sue'] = sue
+tom = db['tom']
+tom.giveRaise(.20)
+db['tom'] = tom
+db.close()
